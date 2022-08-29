b09e01d36c84244ed3f7091075b6a41d93c6b989
johntownsend99 <jtownsend1799@gmail.com>
2022-08-29 16:02:22 -0600

    updated scraper to get top players drafted in each position over the past five years. Includes PPR and standard scoring data.

--- scraper.py
+++ scraper.py
@@ -1,28 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
-url = 'https://fantasyfootballcalculator.com/adp'
-requests.get(url)
-page = requests.get(url)
+years = ['2017','2018','2019','2020','2021','2022']
+league_type = ['standard', 'ppr']
+league_size = ['8-team','10-team','12-team','14-team']
+position = ['quarterback','running-back','wide-receiver','tight-end','defense','kicker']
+DIR_PATH = os.path.join(os.getcwd(), "scraper_data")
 
-soup = BeautifulSoup(page.text, 'lxml')
-#print(soup.prettify())
-table_data = soup.find('table', class_ ="table adp")
+for yr in years:
+    for lt in league_type:
+        for ls in league_size:
+            for pos in position:
+                url = 'https://fantasyfootballcalculator.com/adp/{0}/{1}/{2}/{3}'.format(lt, ls, pos, yr)
+                
+                #csv_formatter = '{0}/{1}/{2}/{3}'.format(lt, ls, pos, yr)
+                #csv_list.append(csv_formatter)
 
-headers = []
-for i in table_data.find_all('th'):
-    title = i.text
-    headers.append(title)
+                requests.get(url)
+                page = requests.get(url)
 
-#print(headers)
+                soup = BeautifulSoup(page.text, 'lxml')
+                table_data = soup.find('table', class_ ="table adp")
 
-df = pd.DataFrame(columns = headers)
+                headers = []
+                for i in table_data.find_all('th'):
+                    title = i.text
+                    headers.append(title)
 
-for j in table_data.find_all('tr')[1:]:
-    row_data = j.find_all('td')
-    row = [tr.text for tr in row_data]
-    length = len(df)
-    df.loc[length] = row
 
-print(df)
+                df = pd.DataFrame(columns = headers)
+
+                for j in table_data.find_all('tr')[1:]:
+                    row_data = j.find_all('td')
+                    row = [tr.text for tr in row_data]
+                    length = len(df)
+                    df.loc[length] = row
+        
+                FILE_PATH = os.path.join(DIR_PATH, "{0}-{1}-{2}-{3}.csv".format(lt, ls, pos, yr))
+                df.to_csv(FILE_PATH)
+    
